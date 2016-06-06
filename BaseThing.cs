32e126d657971f9ff861587f2440b5cfe5022c85
Tim Abell <tim@timwise.co.uk>
2016-06-06 18:22:42 +0100

    Basic test of basic base class

---
+++ BaseThing.cs
@@ -0,0 +1,17 @@
+﻿namespace resharper_refactor_test
+{
+    public class BaseThing
+    {
+        public int CallCount { get; set; }
+
+        public BaseThing()
+        {
+            CallCount = 0;
+        }
+
+        public void CallMeSometime()
+        {
+            CallCount++;
+        }
+    }
+}
---
+++ Tessts.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace resharper_refactor_test
+{
+    [TestFixture]
+    class Tessts
+    {
+        /// <summary>
+        /// Sanity check
+        /// </summary>
+        [Test]
+        public void TestBaseThing()
+        {
+            var subject = new BaseThing();
+            subject.CallMeSometime();
+            subject.CallCount.Should().Be(1);
+        }
+    }
+}
