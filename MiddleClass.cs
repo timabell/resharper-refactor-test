fc33ec3f242cdf61d7280a7b8e2de1e88667240a
Tim Abell <tim@timwise.co.uk>
2016-06-06 18:40:59 +0100

    Remove the redundant method manually
    No resharper involvement, no broken code.

--- MiddleClass.cs
+++ MiddleClass.cs
@@ -6,9 +6,5 @@ namespace resharper_refactor_test
     {
         // resharper thinks this is redundant and offers to remove it, which it is,
         // *but* it also removes the line from upper, *breaking* the call to the base from upper
-        public override void CallMeSometime()
-        {
-            base.CallMeSometime();
-        }
     }
 }
